f10ccc0fc36516d5657c7e99b20aee991c2a4218
Sebastian Sonntag <se.bastian.sonntag@hotmail.com>
2019-12-29 22:28:01 +0100

    add header

---
+++ analyze_auger_data.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+#
+# ******************************************************************************
+# Title: analyze_auger_data.py
+# Description: Read Auger public data and analyze accordingly
+# Author: Sebastian Sonntag
+# Date: 2019-12-29
+# License:
+# ******************************************************************************
