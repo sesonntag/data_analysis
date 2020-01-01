d096ab6ae712a076d9637e7d64bccf7619e37066
Sebastian Sonntag <se.bastian.sonntag@hotmail.com>
2020-01-02 00:35:41 +0100

    cast unix_time to cet_time and do some test plots

--- analyze_auger_data.py
+++ analyze_auger_data.py
@@ -4,14 +4,21 @@
 # Title: analyze_auger_data.py
 # Description: Read Auger public data and analyze accordingly
 # Author: Sebastian Sonntag
-# Date: 2019-12-29
+# Date: 2020-01-02
 # License:
 # ******************************************************************************
 
 import pandas as pd
+from datetime import datetime
+import matplotlib.pyplot as plt
 
 df = pd.read_csv('auger_public_2019_12_29.txt', sep=' ', skiprows=19,
                  names=['event_id', 'n_stations', 'angle_theta', 'angle_phi',
                         'energy', 'unix_time', 'galactic_longitude',
                         'galactic_latitude'])
-print(df.head())
+
+df['cet_time'] = pd.to_datetime(df['unix_time'], unit='s')
+
+#plt.scatter(df['angle_phi'], df['angle_theta'])
+plt.hist(df['angle_theta'], bins=20)
+plt.show()
